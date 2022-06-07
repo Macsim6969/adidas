5b698dd943c7a191f0aada073ada270dcde70691
Максим Белоусов <Macs>
2022-06-07 21:17:56 +0300

    060700

--- src/components/Mens/Shoes/MenDetails.jsx
+++ src/components/Mens/Shoes/MenDetails.jsx
@@ -4,12 +4,12 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 import { setSneackGallery, setThing, setThings } from '../../../redux/action/nemshoes-action'
 
- 
+
 const MenDetails = () => {
     const dispatch = useDispatch()
     const { id } = useParams()
-    const {sneakers, gallery} = useSelector((state) => state.sneak)
-   console.log(gallery)
+    const { sneakers, gallery } = useSelector((state) => state.sneak)
+    console.log(gallery)
 
     useEffect(() => {
         axios.get(`https://6293babe7aa3e6af1a102469.mockapi.io/adidas/menshoes/menshoes?id=${id}`).then(resp => {
@@ -23,8 +23,11 @@ const MenDetails = () => {
             <Link to={'/men-sneakers'} >Back</Link>
             <h1>Sneakers</h1>
             {
-                gallery && gallery.map(e =>{
-                    return <img src={e} alt="" />
+                gallery && gallery.map(e => {
+                    return <>
+                        <img src={e} alt="" />
+                        <nav>{e}</nav>
+                    </>
                 })
             }
         </div>
